cd39e763ffd9d15545b6f44a187fee242ce62b4b
RainbowZap223 <79471824+RainbowZap223@users.noreply.github.com>
2021-08-02 19:40:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ function gotResult(error, results) {
   else {
     console.log(results);
     document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].label.toFixed(3);
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
   }
 }
